fb963544e19b027ec0d61416ec84b3c7cb241e3a
Khrapenkov <117178211+Khrapenkov@users.noreply.github.com>
2022-11-15 18:33:34 +0300

    Create example.py
    создал файл для 1 игры

---
+++ lab8/example.py
@@ -0,0 +1,51 @@
+import pygame
+from pygame.draw import *
+from random import randint
+pygame.init()
+
+FPS = 1.5
+screen = pygame.display.set_mode((1200, 900))
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+BLACK = (0, 0, 0)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
+def new_ball():
+    global x, y, r
+    x = randint(100,700)
+    y = randint(100,500)
+    r = randint(30,50)
+    color = COLORS[randint(0, 5)]
+    circle(screen, color, (x, y), r)
+
+def click(event):
+    print(x, y, r)
+
+
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+			print('Click!')
+    new_ball()
+    if (event.pos[0] - x)**2 + (event.pos[1] - y)**2 > (x-r)**2+(y-r)**2:
+				
+			elif:
+				finished = True
+    pygame.display.update()
+    screen.fill(BLACK)
+
+pygame.quit()
